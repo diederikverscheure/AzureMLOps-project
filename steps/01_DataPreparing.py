7842bead3949b3a08db67f68ea98392194574390
dve <diederik.verscheure@duco.eu>
2022-01-26 15:01:53 +0100

    Check where the file runs.

--- steps/01_DataPreparing.py
+++ steps/01_DataPreparing.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from steps.utils import connectWithAzure
+#from steps.utils import connectWithAzure
 from azureml.core import Dataset
 from azureml.data.datapath import DataPath
 from sklearn.model_selection import TimeSeriesSplit
