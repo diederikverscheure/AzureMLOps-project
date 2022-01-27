e41e3c67bf37127dc6b61002cef66f36ffc0aa2e
dve <diederik.verscheure@duco.eu>
2022-01-27 14:24:20 +0100

    End to end deployment.

--- scripts/score.py
+++ scripts/score.py
@@ -4,7 +4,6 @@ import pandas as pd
 import json
 import pickle
 from scipy.signal import savgol_filter
-import matplotlib.pyplot as plt
 
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score
 from sklearn.linear_model import LogisticRegression
@@ -34,24 +33,21 @@ def run(json_data):
     likelyhood = model.predict_proba(X)
     return 'Presence likelihood = ' + str(likelyhood[:,1])
 
-def plot_decision_boundary():
-    xx, yy = np.mgrid[0:3000, -5:5:.01]
-    grid = np.c_[xx.ravel(), yy.ravel()]
-    probs = model.predict_proba(grid)[:, 1].reshape(xx.shape)
+# def plot_decision_boundary():
+#     xx, yy = np.mgrid[0:3000, -5:5:.01]
+#     grid = np.c_[xx.ravel(), yy.ravel()]
+#     probs = model.predict_proba(grid)[:, 1].reshape(xx.shape)
     
-    f, ax = plt.subplots(figsize=(8, 6))
-    contour = ax.contourf(xx, yy, probs, 25, cmap="RdBu",
-                          vmin=0, vmax=1)
-    ax_c = f.colorbar(contour)
-    ax_c.set_label("$P(y = 1)$")
-    ax_c.set_ticks([0, .25, .5, .75, 1])
+#     f, ax = plt.subplots(figsize=(8, 6))
+#     contour = ax.contourf(xx, yy, probs, 25, cmap="RdBu",
+#                           vmin=0, vmax=1)
+#     ax_c = f.colorbar(contour)
+#     ax_c.set_label("$P(y = 1)$")
+#     ax_c.set_ticks([0, .25, .5, .75, 1])
     
-    ax.set(xlim=(0, 3000), ylim=(-5, 5),
-           xlabel="CO2", ylabel="CO2 derivative")
+#     ax.set(xlim=(0, 3000), ylim=(-5, 5),
+#            xlabel="CO2", ylabel="CO2 derivative")
 
-
-def getTargets(df):
-    return np.hstack((df['presence_living'].values,df['presence_bedroom'].values))
 
 def smooth(index,y,derivative=1):
     dt = (index[1:]-index[0:-1]).total_seconds()
@@ -62,12 +58,3 @@ def getScoreFeatures(df):
     df['deriv'] = smooth(df.index,df.iloc[:,0]-400,derivative=1)
     return df.iloc[:,0:2].values
     
-def getFeatures(df):
-    df['delta_living']= df['co2_living']-df['co2_outside']
-    df['delta_bedroom'] = df['co2_bedroom']-df['co2_outside']
-    df['deriv_living'] = smooth(df.index,df['co2_living'],derivative=1)
-    df['deriv_bedroom'] = smooth(df.index,df['co2_bedroom'],derivative=1)
-    return np.vstack((df[['delta_living','deriv_living']].values,
-                      df[['delta_bedroom','deriv_bedroom']].values))
-
-
